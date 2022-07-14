6326dd5234eac1ab05a188d6a816d4b2d778131f
unknown <marciors92@gmail.com>
2022-07-14 17:08:11 -0300

    Adicionando relógio

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,26 @@ let emailOk = false
 let assuntoOk = false
 let mapa = document.querySelector("#mapa")
 
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time(){
+    let dateToday = new Date();
+    let h = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    if (h < 10) h = '0' + h;
+    if (min < 10) min = '0' + min;
+    if (s < 10) s = '0' + s;
+
+    horas.textContent = h;
+    minutos.textContent = min;
+    segundos.textContent = s;
+
+})
+
 nome.style.width = '100%'
 email.style.width = '100%'
 
